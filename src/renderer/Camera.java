bb43a1fa5dbda31dacf18fad2bc1b43227c7eed1
Khelkhely <rachel.shapiro.beitel@gmail.com>
2024-06-26 21:13:44 +0300

    finished tests for integration

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -6,17 +6,27 @@ import primitives.Util;
 import primitives.Vector;
 
 import java.util.MissingResourceException;
-
 import static primitives.Util.isZero;
 
+/**
+ * Class Camera is the basic class representing a camera in Cartesian 3-Dimensional coordinate system.
+ * @author Rachel and Tehila
+ */
 public class Camera implements Cloneable {
+    /** a point that representing the camera's location **/
     private Point p0;
+    /** a vector representing the up direction of the camera **/
     private Vector vUp;
+    /** a vector representing the direction the camera is facing **/
     private Vector vTo;
+    /** a vector representing the right direction of the camera **/
     private Vector vRight;
 
+    /** the width of the view plane*/
     private double viewPlaneWidth = 0;
+    /** the height of the view plane*/
     private double viewPlaneHeight = 0;
+    /** the distance form the camera to the view plane*/
     private double viewPlaneDistance = 0;
 
     //private ImageWriter imageWriter;
@@ -24,17 +34,112 @@ public class Camera implements Cloneable {
 
     //private Point viewPlanePC;
 
+    /**
+     *
+     */
     private Camera() {
     }
 
+    /**
+     * getter function for viewPlaneDistance
+     * @return the distance between the camera and the view plane
+     */
+    public double getDistance() {
+        return viewPlaneDistance;
+    }
+
+    /**
+     * getter function for viewPlaneHeight
+     * @return the height of the view plane
+     */
+    public double getHeight() {
+        return viewPlaneHeight;
+    }
+
+    /**
+     * getter function for viewPlaneWidth
+     * @return the width of the view plane
+     */
+    public double getWidth() {
+        return viewPlaneWidth;
+    }
+
+    /**
+     * getter function for camera's vRight
+     * @return the vector representing the camera's Right direction
+     */
+    public Vector getvRight() {
+        return vRight;
+    }
+
+    /**
+     * getter function for camera's vTo
+     * @return the vector representing the camera's To direction
+     */
+    public Vector getvTp() {
+        return vTo;
+    }
+
+    /**
+     * getter function for camera's vUp
+     * @return the vector representing the camera's Up direction
+     */
+    public Vector getvUp() {
+        return vUp;
+    }
+
+    /**
+     * getter function for camera's p0
+     * @return the point representing the camera's location
+     */
+    public Point getP0() {
+        return p0;
+    }
+
+    /**
+     * a function that returns a builder object which can be used to construct a camera
+     * @return a builder object for camera
+     */
+    public static Builder getBuilder() {
+        return null;
+    }
+
+    /**
+     * creates a ray from the camera to the view plane that goes through the specified pixel in the view plane
+     * @param nX the number of columns on the view plane
+     * @param nY the number of rows on the view plane
+     * @param j the index of the column of the pixel on the view plane
+     * @param i the index of the row of the pixel on the view plane
+     * @return a ray that starts at the camera and goes through the specified pixel on the view plane
+     */
+    public Ray constructRay(int nX, int nY, int j, int i) {
+        return null;
+    }
+
+    /**
+     * a class of objects to construct a camera
+     * @author Rachel and Tehila
+     */
     public static class Builder {
         final private Camera camera = new Camera();
 
+        /**
+         * setter function for camera location
+         * @param location the point that represents the camera's location
+         * @return the camera object with the updated location
+         */
         public Builder setLocation(Point location) {
             camera.p0 = location;
             return this;
         }
 
+        /**
+         * setter function for the direction of the camera
+         * @param to the vector that represents the direction the camera is facing
+         * @param up the vector that represents the up direction of the camera
+         * @return the camera object with the updated direction vectors
+         * @throws IllegalArgumentException if the vectors to and up are orthogonal to each other
+         */
         public Builder setDirection(Vector to, Vector up) {
             if(!isZero(to.dotProduct(up))) {
                 throw new IllegalArgumentException("Vectors to and up must be orthogonal to each other.");
@@ -44,6 +149,13 @@ public class Camera implements Cloneable {
             return this;
         }
 
+        /**
+         * a setter function for the size of the view plane
+         * @param width the width of the view plane
+         * @param height the height of the view plane
+         * @return the camera object with the updated view plane size
+         * @throws IllegalArgumentException if the width or height are not positive
+         */
         public Builder setVpSize(double width, double height) {
             if (Util.alignZero(width) <= 0 || Util.alignZero(height) <= 0) {
                 throw new IllegalArgumentException("Width and height must be positive numbers.");
@@ -53,6 +165,12 @@ public class Camera implements Cloneable {
             return this;
         }
 
+        /**
+         * a setter function for the distance between the camera and the view plane
+         * @param distance the distance between the camera and the view plane
+         * @return the camera object with the updated distance
+         * @throws IllegalArgumentException if the distance is not positive
+         */
         public Builder setVpDistance(double distance) {
             if (Util.alignZero(distance) <= 0) {
                 throw new IllegalArgumentException("Distance must be positive numbers.");
@@ -71,6 +189,10 @@ public class Camera implements Cloneable {
             return this;
         }*/
 
+        /***
+         * checks that all the necessary data is set for the camera and is valid, and calculates the missing data
+         * @return a copy of the camera object that was built
+         */
         public Camera build() {
             String generalDescription = "Rendering data missing.";
             String className = "Camera";
@@ -92,47 +214,24 @@ public class Camera implements Cloneable {
             if (Util.alignZero(camera.viewPlaneDistance) <= 0) {
                 throw new IllegalArgumentException("Plane view distance must be positive.");
             }
+
+            if (!isZero(camera.vTo.length() - 1) ||
+                    !isZero(camera.vUp.length() - 1) ||
+                    !isZero(camera.vRight.length() - 1)) {
+                throw new IllegalArgumentException("Vectors must be normalized");
+            }
             if (!isZero(camera.vRight.dotProduct(camera.vTo))) {
                 throw new IllegalArgumentException("Vectors to and up must be orthogonal to each other.");
             }
             camera.vRight = camera.vTo.crossProduct(camera.vUp).normalize();
-            return (Camera) camera;//.clone();
+
+            try{
+                return (Camera) camera.clone();
+            }
+            catch(CloneNotSupportedException ex) {
+                throw new UnsupportedOperationException("cloning unsupported");
+            }
+
         }
-    }
-
-    /*public static Builder getBuilder() {
-
-    }*/
-
-    public Ray constructRay(int nX, int nY, int j, int i) {
-        return null;
-    }
-
-    /*public double getDistance() {
-        return distance;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public double getWidth() {
-        return width;
-    }*/
-
-    public Vector getvRight() {
-        return vRight;
-    }
-
-    /*public Vector getvTp() {
-        return vTp;
-    }*/
-
-    public Vector getvUp() {
-        return vUp;
-    }
-
-    public Point getP0() {
-        return p0;
     }
 }
---
+++ unittests/renderer/RayConstrutIntersectTest.java
@@ -0,0 +1,157 @@
+package renderer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import geometries.Intersectable;
+import geometries.Plane;
+import geometries.Sphere;
+import geometries.Triangle;
+import org.junit.jupiter.api.Test;
+
+import primitives.*;
+import renderer.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Testing integration between the ray construction and ray intersections functions
+ * @author Rachel and Tehila
+ */
+public class RayConstrutIntersectTest {
+
+    /**
+     * counts the number of intersection points between a geometry and the rays that are cast from
+     * a camera to a 3x3 view plane
+     * @param geometry the geometry that the rays intersect
+     * @param camera the camera the rays are cast from
+     * @return the number of intersections
+     */
+    int countIntersections(Intersectable geometry, Camera camera) {
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                List<Point> list = geometry.findIntersections(camera.constructRay(3,3,j, i));
+                if(list != null) {
+                    count += list.size();
+                }
+            }
+        }
+        return count;
+    }
+
+    @Test
+    void testSphereIntegration() {
+        Camera camera1 = Camera.getBuilder()
+                .setVpSize(3,3)
+                .setVpDistance(1)
+                .setDirection(new Vector(0,0,-1), new Vector(0,1,0))
+                .setLocation(new Point(0,0,0))
+                .build();
+
+        // TC01
+        Sphere sphere1 = new Sphere(1, new Point(0,0,-3));
+
+        assertEquals(2,
+                countIntersections(sphere1, camera1),
+                "Number of intersection points is incorrect.");
+
+        // TC02
+        Camera camera2 = Camera.getBuilder()
+                .setVpSize(3,3)
+                .setVpDistance(1)
+                .setDirection(new Vector(0,0,-1), new Vector(0,1,0))
+                .setLocation(new Point(0,0,0.5))
+                .build();
+        Sphere sphere2 = new Sphere(2.5, new Point(0,0,-2.5));
+        assertEquals(18,
+                countIntersections(sphere2, camera2),
+                "Number of intersection points is incorrect.");
+
+        // TC03
+        Sphere sphere3 = new Sphere(2, new Point(0,0,-2));
+        assertEquals(10,
+                countIntersections(sphere2, camera2),
+                "Number of intersection points is incorrect.");
+
+        // TC04
+        Sphere sphere4 = new Sphere(4, new Point(0,0,-2));
+        assertEquals(9,
+                countIntersections(sphere4, camera2),
+                "Number of intersection points is incorrect.");
+
+        // TC05
+        Sphere sphere5 = new Sphere(0.5, new Point(0,0,1));
+        assertEquals(0,
+                countIntersections(sphere5, camera1),
+                "Number of intersection points is incorrect.");
+    }
+
+    @Test
+    void testTriangleIntegration() {
+        Camera camera = Camera.getBuilder()
+                .setVpSize(3,3)
+                .setVpDistance(1)
+                .setDirection(new Vector(0,0,-1), new Vector(0,1,0))
+                .setLocation(new Point(0,0,0))
+                .build();
+
+        // TC01
+        Triangle triangle1 = new Triangle(
+                new Point(0,1,-2),
+                new Point(1,-1,-2),
+                new Point(-1,-1,-2)
+        );
+        assertEquals(1,
+                countIntersections(triangle1, camera),
+                "Number of intersection points is incorrect.");
+
+        // TC02
+        Triangle triangle2 = new Triangle(
+                new Point(0,20,-2),
+                new Point(1,-1,-2),
+                new Point(-1,-1,-2)
+        );
+        assertEquals(2,
+                countIntersections(triangle2, camera),
+                "Number of intersection points is incorrect.");
+    }
+
+    @Test
+    void testPlaneIntegration() {
+        Camera camera = Camera.getBuilder()
+                .setVpSize(3,3)
+                .setVpDistance(1)
+                .setDirection(new Vector(0,0,-1), new Vector(0,1,0))
+                .setLocation(new Point(0,0,0))
+                .build();
+
+        // TC01
+        Plane plane1 = new Plane(
+          new Point(0,0,-3),
+          new Vector(0,0,1)
+        );
+        assertEquals(9,
+                countIntersections(plane1, camera),
+                "Number of intersection points is incorrect.");
+
+        // TC02
+        Plane plane2 = new Plane(
+                new Point(0,0,-3),
+                new Vector(0,-1,1)
+        );
+        assertEquals(9,
+                countIntersections(plane2, camera),
+                "Number of intersection points is incorrect.");
+
+        // TC03
+        Plane plane3 = new Plane(
+                new Point(0,0,-3),
+                new Vector(0,-9,1)
+        );
+        assertEquals(6,
+                countIntersections(plane3, camera),
+                "Number of intersection points is incorrect.");
+
+    }
+}
